02c0feb473dd087915e6381f058b4a3d354ac0e5
Irina <golubkovairina76@gmail.com>
2022-03-23 21:53:20 +0300

    Changing a Job

--- src/main/java/PedometerManager.java
+++ src/main/java/PedometerManager.java
@@ -4,15 +4,34 @@ import java.util.Map;
 public class PedometerManager {
     private final Map<Integer, Integer> data = new HashMap<>();
 
-    public void addSteps(int day, int steps) {
+    public int addSteps(int day, int steps) {
         if (data.containsKey(day)) {
             data.put(day, data.get(day) + steps);
         } else {
             data.put(day, steps);
         }
+        return getCountOfStepsToExceedMaximum(day);
     }
 
-    public int getCountOfStepsToExceedDayMaximum(int day) {
-        return data.getOrDefault(day, 0) + 1;
+    private int getCountOfStepsToExceedMaximum(int day) {
+        int maxSteps = calculateMaxOfSteps();
+        int stepsInDay = data.getOrDefault(day, 0);
+        return maxSteps - stepsInDay + 1;
+    }
+
+    private int calculateMaxOfSteps() {
+        int max = 0;
+        for (int val : data.values()) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
     }
 }
+//    Пояснение: метод add должен добавлять/перезаписывать значение шагов,
+//        анализировать максимум и выводить количество оставшихся шагов до превышения максимума
+
+    //Создайте менеджер шагометра с методом `add(int day, int steps)`, который не перезаписывает значение за день,
+// а добавляет к уже текущему значению. Пусть менеджер возвращает число, равное количеству шагов, которое надо сделать в этот день
+// чтобы обновить превысить текущий максимум шагов за день.
--- src/test/java/PedometerManagerTest.java
+++ src/test/java/PedometerManagerTest.java
@@ -1,36 +1,18 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PedometerManagerTest {
-    PedometerManager pedometerManager = new PedometerManager();
-    {
-        pedometerManager.addSteps(1, 1000);
-        pedometerManager.addSteps(1, 2000);
-        pedometerManager.addSteps(1, 3000);
-        pedometerManager.addSteps(2, 5000);
-        pedometerManager.addSteps(3, 500);
-        pedometerManager.addSteps(3, 600);
-        pedometerManager.addSteps(1, 500);
 
-    }
+    @Test
+    public void shouldCalculateCountOfStepsToExceedADayMaximum() {
+        PedometerManager pedometerManager = new PedometerManager();
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, 6501",
-            "2, 5001",
-            "3, 1101",
-            "4, 1",
-    })
-    public void shouldCalculateCountOfStepsToExceedADayMaximum(
-            int givenDay,
-            int expectedCountOfSteps
-    ) {
-        // when
-        int actualCountOfSteps = pedometerManager.getCountOfStepsToExceedDayMaximum(givenDay);
+        assertEquals(1, pedometerManager.addSteps(1, 1000)); // 1=1000
+        assertEquals(1, pedometerManager.addSteps(1, 1000)); // 1=2000
+        assertEquals(1501, pedometerManager.addSteps(2, 500)); //1=2000,2=500
+        assertEquals(1, pedometerManager.addSteps(3, 3000)); //1=2000,2=500,3=3000
+        assertEquals(501, pedometerManager.addSteps(1, 500)); //1=2500,2=500,3=3000
 
-        // then
-        Assertions.assertEquals(expectedCountOfSteps, actualCountOfSteps);
     }
 
 
